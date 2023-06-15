9723683c4d40b1b29a389019aaee58d4c2d4c346
Victorine2 <vickgachuhi@gmail.com>
2023-06-15 16:51:06 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - To print a new line using the printf function
+ * Return: 0 Is a success.
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
