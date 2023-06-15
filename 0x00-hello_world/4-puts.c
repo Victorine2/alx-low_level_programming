d0c531bf27289a46bf88f771fb72a292c425f089
Victorine2 <vickgachuhi@gmail.com>
2023-06-15 16:33:43 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - using the puts fuction to print a sentence
+ * Return: when output is 0, the input is right.
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
