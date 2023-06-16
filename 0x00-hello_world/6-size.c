84ea4bf038a802109378f716c7ea1889c2aedecf
Victorine2 <vickgachuhi@gmail.com>
2023-06-15 18:16:21 -0700

    6-size.c git commit -m 6-size.c
    clear
    
    git commit -m 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - that prints the size of various types on the computer it is compiled and run on
+ * Return - when result is 0, its a success.
+ */
+int main(void)
+}
+	printf("Size of an int: %d byte(s)", sizeof(int));
+	printf("Size of a char: %d byte(s)", sizeof(char));
+	printf("Size of a float: %d byte(s)", sizeof(float));
+	printf("Size of a long int: %d byte(s)", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)", sizeof(long long int));
+	return (0);
+}
