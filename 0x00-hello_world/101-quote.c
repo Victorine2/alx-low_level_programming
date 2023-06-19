4b4de74bfb4b2955917254952d676b02023ef16a
Victorine2 <vickgachuhi@gmail.com>
2023-06-19 10:45:12 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * main - Prints a statement followed by a new line to standard error
+ * Return: 1 Is a success
+ */
+
+int main(void)
+{
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
