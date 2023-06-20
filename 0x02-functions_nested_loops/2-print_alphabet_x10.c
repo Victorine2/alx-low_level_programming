c1638707691498732fa123ee5c2eaaf07921e913
Victorine2 <vickgachuhi@gmail.com>
2023-06-20 12:26:43 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,9 +11,9 @@ void print_alphabet_x10(void)
 
 	int b;
 
-	for (b < 10; a <= 10; a++)
+	for (b < 1; a <= 10; a++)
 	{
-		for (a = 97; a <= 122; a++)
+		for (b = 97; b <= 122; b++)
 		{
 			_putchar(b);
 		}
