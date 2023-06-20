cb1b37bd6eebe0c0abbe4380276add0c169f85dd
Victorine2 <vickgachuhi@gmail.com>
2023-06-20 12:22:36 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabets 10 times
+ * return: 0 is a success
+ */
+
+void print_alphabet_x10(void)
+{
+	int a;
+
+	int b;
+
+	for (b < 10; a <= 10; a++)
+	{
+		for (a = 97; a <= 122; a++)
+		{
+			_putchar(b);
+		}
+		_putchar('\n');
+	}
+
+}
